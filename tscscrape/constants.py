87158083c5dd61191fd739ed4aa7f0c61731ddd3
z33kz33k <zeek@int.pl>
2019-01-20 14:07:05 +0100

    Finish 'scrape_allcities()'

---
+++ tscscrape/constants.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "http://www.skyscrapercenter.com/compare-data/submit?type%5B%5D=building&status%5B%5D=COM&status%5B%5D=UC&status%5B%5D=UCT&status%5B%5D=STO&base_region=0&base_country=0&base_city={}&base_height_range={}&base_company=All&base_min_year=1900&base_max_year=9999&comp_region=0&comp_country=0&comp_city=0&comp_height_range=4&comp_company=All&comp_min_year=1960&comp_max_year=2020&skip_comparison=on&output%5B%5D=list&dataSubmit=Show+Results"
+
+
+def _scrape_citycodes():
+    """Scrape available city codes into a dict to be saved as CITIES_CODES constant"""
+    contents = requests.get(URL.format("All", "10")).text
+    soup = BeautifulSoup(contents, "lxml")
+    result = soup.find("select", id="base_city")
+    result = result.find_all("option")
+    return {tag.string: tag["value"] for tag in result}
+
+
+def _scrape_heightranges():
+    """Scrape available height ranges into a dict to be saved as HEIGHT_RANGES constant"""
+    contents = requests.get(URL.format("All", "10")).text
+    soup = BeautifulSoup(contents, "lxml")
+    result = soup.find("select", id="base_height_range")
+    result = result.find_all("option")
+    return {tag.string: tag["value"] for tag in result}
+
+
+# keys of below dicts are the same as options in "Base Data Range" form on the website
+CITIES_CODES = _scrape_citycodes()
+HEIGHT_RANGES = _scrape_heightranges()
--- tscscrape/scraper.py
+++ tscscrape/scraper.py
@@ -4,33 +4,8 @@ import json
 import time
 from pprint import pprint
 
+from tscscrape.constants import URL, CITIES_CODES, HEIGHT_RANGES
 from tscscrape.errors import PageWrongFormatError
-
-
-URL = "http://www.skyscrapercenter.com/compare-data/submit?type%5B%5D=building&status%5B%5D=COM&status%5B%5D=UC&status%5B%5D=UCT&status%5B%5D=STO&base_region=0&base_country=0&base_city={}&base_height_range={}&base_company=All&base_min_year=1900&base_max_year=9999&comp_region=0&comp_country=0&comp_city=0&comp_height_range=4&comp_company=All&comp_min_year=1960&comp_max_year=2020&skip_comparison=on&output%5B%5D=list&dataSubmit=Show+Results"
-
-
-def _scrape_citycodes():
-    """Scrape available city codes into a dict to be saved as CITIES_CODES constant"""
-    contents = requests.get(URL.format("All", "10")).text
-    soup = BeautifulSoup(contents, "lxml")
-    result = soup.find("select", id="base_city")
-    result = result.find_all("option")
-    return {tag.string: tag["value"] for tag in result}
-
-
-def _scrape_heightranges():
-    """Scrape available height ranges into a dict to be saved as HEIGHT_RANGES constant"""
-    contents = requests.get(URL.format("All", "10")).text
-    soup = BeautifulSoup(contents, "lxml")
-    result = soup.find("select", id="base_height_range")
-    result = result.find_all("option")
-    return {tag.string: tag["value"] for tag in result}
-
-
-# keys of below dicts are the same as options in "Base Data Range" form on the website
-CITIES_CODES = _scrape_citycodes()
-HEIGHT_RANGES = _scrape_heightranges()
 
 
 def scrape_city(city, height_range="All", trim_heightless=True, floor=None):
@@ -43,7 +18,7 @@ def scrape_city(city, height_range="All", trim_heightless=True, floor=None):
         result = next(resultset for resultset in soup.find_all("script", type="text/javascript")
                       if keystr in resultset.text)
     except StopIteration:
-        raise ValueError(
+        raise PageWrongFormatError(
             "Page for '{}' seems to have wrong format (missing '{}' string).\nFull URL: {}".format(city, keystr, url))
     result = result.text.strip()
     result = "".join(result.split(keystr)[1:])[:-1]
@@ -62,19 +37,19 @@ def scrape_allcities(height_range="All", trim_heightless=True, floor=None):
     """Scrape all cities data and dump it to file"""
     cities = {}
     for i, key in enumerate(key for key in CITIES_CODES.keys() if key != "All"):
-        if i < 20:
+        try:
             towers = scrape_city(key, height_range, trim_heightless, floor)
-            if towers:
-                cities.update({key: towers})
-            print("{}: Scraped {} {} for '{}'...".format(
-                str(i + 1).zfill(4),
-                str(len(towers)),
-                "towers" if len(towers) != 1 else "tower",
-                key
-            ))
-            time.sleep(0.02)
-        else:
-            break
+        except PageWrongFormatError:
+            towers = []
+        if towers:
+            cities.update({key: towers})
+        print("{}: Scraped {} {} for '{}'...".format(
+            str(i + 1).zfill(4),
+            str(len(towers)),
+            "towers" if len(towers) != 1 else "tower",
+            key
+        ))
+        time.sleep(0.02)
 
     with open("output/cities.json", mode="w") as jsonfile:
         json.dump(cities, jsonfile)
